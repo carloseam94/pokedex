d90d6dc8474a45c688e44ef1815c3f041d0a1bd4
vplasencia <v.pcalana@gmail.com>
2021-05-16 13:09:10 -0400

    added repository link in footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,6 +4,13 @@
       class="text-center p-3 text-white"
       style="background-color: rgba(0, 0, 0, 0.2)"
     >
+      <a
+        href="https://github.com/carloseam94/pokedex"
+        rel="noreferrer noopener nofollow"
+        target="_blank"
+        ><i class="fab fa-github"></i
+      ></a>
+
       <h6 class="my-4">
         Made with
         <span class="text-danger mx-1">
@@ -13,18 +20,25 @@
         <a
           class="link-light"
           style="text-decoration: underline"
-          href="https://github.com/vplasencia"
+          rel="noreferrer noopener nofollow"
+          href="https://twitter.com/ViviPlasenciaC?s=09"
+          title="Vivian Twitter Profile"
+          target="_blank"
           >Vivian</a
         >
         and
         <a
           class="link-light"
           style="text-decoration: underline"
+          rel="noreferrer noopener nofollow"
           href="https://github.com/carloseam94"
+          title="Carlos Github Profile"
+          target="_blank"
           >Carlos</a
         >
       </h6>
     </div>
+    <div></div>
   </footer>
 </template>
 
@@ -32,5 +46,13 @@
 export default {};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.fa-github {
+  font-size: 1.5em;
+  color: white;
+}
+
+.fa-github:hover {
+  color: rgba($color: #fff, $alpha: 0.7);
+}
 </style>
